7e8d7c2d5fbd9ce6d1b85015227b6370b73888d0
arlechann <dragnov3728@gmail.com>
2022-04-15 02:10:15 +0900

    Refactor login function

--- backend/app.rb
+++ backend/app.rb
@@ -5,77 +5,62 @@ require 'json'
 require 'sequel'
 require 'bcrypt'
 
-def login_user
-  session[:user_id]
-end
+require_relative 'lib/login'
 
-def logged_in?
-  login_user()
-end
+DB = Sequel.connect('postgres://postgres:password@db:5432/blog')
 
-configure do
-  enable :sessions
-  #set :settions, expire_after: 60 * 60 * 24 * 30 # 1 month
-  set :settions, expire_after: 60
+use Rack::Session::Cookie,
+  expire_after: 30 * 24 * 60 * 60, # 30 days
+  secret: ''
 
-  DB = Sequel.connect('postgres://postgres:password@db:5432/blog')
-end
-
-get '/' do
-  'Hello world!'
-end
-
-get '/admin/' do
-  redirect to('/admin/login') unless logged_in?
-  'Logged-in!'
-end
-
-get '/admin/debug' do
-  data = {
-    'session' => session.to_s,
-  }
-  erb :'admin/debug', :locals => { data: data }
-end
-
-get '/admin/login' do
-  redirect to('/admin/') if logged_in?
-  default = { message: '', email: '' }
-  erb :'admin/login', :locals => default.merge(params)
-end
-
-post '/admin/login' do
-  email = params[:email]
-  password = params[:password]
-
-  failed_url = '/admin/login?' + URI.encode_www_form({ message: 'ログイン失敗', email: email })
-
-  record = DB[<<~SQL, email: email].first
+use SessionLogin, {
+  login_path: '/admin/login',
+  logout_path: '/admin/logout',
+  success_path: '/admin/',
+  login_id_key: 'email',
+  form_login_id_key: 'email',
+  failed_message: 'ログイン失敗',
+} do |email|
+  admin = DB[<<~SQL, email: email].first
     SELECT administrators.id AS id, email, password
     FROM administrators
     JOIN administrator_secrets
       ON administrators.id = administrator_secrets.administrator_id
     WHERE email = :email
   SQL
+  admin['user_id'] = admin['email'] unless admin.nil?
+  admin
+end
 
-  redirect to(failed_url) if record.nil?
-
-  id = record[:id]
-  email = record[:email]
-  password_hash = record[:password].strip
-
-  if BCrypt::Password.new(password_hash) == password
-    session[:user_id] = id
-    redirect to('/admin/')
-  else
-    redirect to(failed_url)
+class App < Sinatra::Application
+  session_login
+  
+  get '/' do
+    'Hello world!'
   end
+  
+  get '/admin/' do
+    redirect to('/admin/login') unless login?
+    'Logged-in!'
+  end
+  
+  get '/admin/debug' do
+    data = {
+      'session' => session.to_s,
+    }
+    erb :'admin/debug', :locals => { data: data }
+  end
+  
+  get '/admin/login' do
+    redirect to('/admin/') if login?
+    default = { message: '', email: '' }
+    erb :'admin/login', :locals => default.merge(params)
+  end
+  
+  get '/api/v1/ping' do
+    JSON.generate(status: :success, message: :pong)
+  end
+
+  run! if app_file == $0
 end
 
-get '/admin/logout' do
-  session.delete(:admin_id)
-  redirect to('/admin/login')
-end
-
-get '/api/v1/ping' do
-  JSON.generate(status: :success, message: :pong)
-end
