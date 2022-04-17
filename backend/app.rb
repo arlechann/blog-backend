aa2a27ece1dc8a6b73e7a4f5186c4af375481eec
arlechann <dragnov3728@gmail.com>
2022-04-18 00:05:19 +0900

    Divide Database class to repositories

--- backend/app.rb
+++ backend/app.rb
@@ -6,6 +6,9 @@ require 'time'
 require_relative 'lib/db'
 require_relative 'lib/login'
 require_relative 'model/post'
+require_relative 'repository/administrator_repository'
+require_relative 'repository/post_repository'
+require_relative 'repository/publish_status_repository'
 
 class App < Sinatra::Application
   session_login
@@ -41,11 +44,14 @@ class App < Sinatra::Application
   get '/admin/post' do
     redirect to('/admin/') unless login?
     
+    post_repo = PostRepository.new(DB)
+    ps_repo = PublishStatusRepository.new(DB)
+    admin_repo = AdministratorRepository.new(DB)
     administrator_id = session[:user_id][:id]
-    db = Database.new()
-    posts = db.all_posts
-    publish_statuses = db.all_publish_statuses
-    administrators = db.all_administrators
+
+    posts = post_repo.all
+    publish_statuses = ps_repo.all
+    administrators = admin_repo.all
 
     @title = '投稿一覧'
     erb :'admin/post/index', :locals => {
@@ -59,7 +65,8 @@ class App < Sinatra::Application
   get '/admin/post/add' do
     redirect to('/admin/') unless login?
 
-    publish_statuses = Database.new().all_publish_statuses
+    ps_repo = PublishStatusRepository.new(DB)
+    publish_statuses = ps_repo.all
 
     @title = '投稿作成'
     erb :'admin/post/add', :locals => { post: nil, publish_statuses: publish_statuses }
@@ -68,13 +75,15 @@ class App < Sinatra::Application
   post '/admin/post/add' do
     redirect to('/admin/') unless login?
 
+    post_repo = PostRepository.new(DB)
+
     post = Post.new(
       title: params[:title] || '',
       content: params[:content] || '',
       publish_status_id: params[:publish_status],
       administrator_id: session[:user_id][:id],
     )
-    pk = Database.new().insert_post(post)
+    pk = post_repo.insert(post)
     redirect to('/admin/post/add') if pk.nil?
     redirect to('/admin/post')
   end
@@ -82,12 +91,14 @@ class App < Sinatra::Application
   get '/admin/post/edit/:id' do
     redirect to('/admin/') unless login?
 
+    post_repo = PostRepository.new(DB)
+    ps_repo = PublishStatusRepository.new(DB)
     post_id = params[:id]
     administrator_id = session[:user_id][:id]
-    db = Database.new()
-    post = db.find_post_by_id(post_id)
+
+    post = post_repo.find_by_id(post_id)
     redirect to('/admin/post') unless post.administrator_id == administrator_id
-    publish_statuses = db.all_publish_statuses
+    publish_statuses = ps_repo.all
 
     @title = '投稿編集'
     erb :'admin/post/add', :locals => { post: post, publish_statuses: publish_statuses }
@@ -96,15 +107,16 @@ class App < Sinatra::Application
   post '/admin/post/edit/:id' do
     redirect to('/admin/') unless login?
 
+    post_repo = PostRepository.new(DB)
     post_id = params[:id]
     administrator_id = session[:user_id][:id]
-    db = Database.new()
-    post = db.find_post_by_id(post_id)
+
+    post = post_repo.find_by_id(post_id)
     return [403, {}, '<h1>Forbidden</h1>'] unless post.administrator_id == administrator_id
     post.update_title(params[:title] || '')
     post.update_content(params[:content] || '')
     post.update_publish_status_id(params[:publish_status])
-    updated_row = db.update_post(post)
+    updated_row = post_repo.update(post)
     redirect to(`/admin/post/edit/#{post_id}`) if updated_row.zero?
     redirect to('/admin/post')
   end
@@ -112,12 +124,12 @@ class App < Sinatra::Application
   post '/admin/post/delete/:id' do
     redirect to('/admin/') unless login?
 
+    post_repo = PostRepository.new(DB)
     post_id = params[:id]
     administrator_id = session[:user_id][:id]
-    db = Database.new()
-    post = db.find_post_by_id(post_id)
+    post = post_repo.find_by_id(post_id)
     return [403, {}, '<h1>Forbidden</h1>'] unless post.administrator_id == administrator_id
-    deleted_row = db.delete_post_by_id(post_id)
+    deleted_row = post_repo.delete_by_id(post_id)
     redirect to('/admin/post')
   end
 
--- backend/lib/db.rb
+++ backend/lib/db.rb
@@ -1,22 +1,11 @@
 require 'logger'
 require 'sequel'
 
-require_relative '../model/administrator'
-require_relative '../model/post'
-require_relative '../model/publish_status'
-
 DB ||= Sequel.connect('postgres://postgres:password@db:5432/blog', {
   logger: Logger.new('log/sql.log'),
 })
 
 class Database
-  def all_administrators
-    DB[<<~SQL].all.map { |admin| Administrator.from_h(admin) }
-      SELECT id, email
-      FROM administrators
-    SQL
-  end
-
   def find_administrator_by_email(email)
     DB[<<~SQL, email: email].first
       SELECT administrators.id AS id, email, password
@@ -25,84 +14,5 @@ class Database
         ON administrators.id = administrator_secrets.administrator_id
       WHERE email = :email
     SQL
-  end
-
-  def all_posts
-    DB[<<~SQL].all.map { |post| Post.from_h(post) }
-      SELECT
-        posts.id AS id,
-        title,
-        content,
-        publish_status_id,
-        administrator_id,
-        created_at,
-        last_updated_at
-      FROM posts
-      INNER JOIN publish_statuses
-        ON posts.publish_status_id = publish_statuses.id
-      INNER JOIN administrators
-        ON posts.administrator_id = administrators.id
-    SQL
-  end
-
-  def find_post_by_id(post_id)
-    Post.from_h(DB[<<~SQL, post_id].first)
-      SELECT
-        posts.id AS id,
-        title,
-        content,
-        publish_status_id,
-        administrator_id,
-        created_at,
-        last_updated_at
-      FROM posts
-      INNER JOIN publish_statuses
-        ON posts.publish_status_id = publish_statuses.id
-      INNER JOIN administrators
-        ON posts.administrator_id = administrators.id
-      WHERE posts.id = ?
-    SQL
-  end
-
-  def all_publish_statuses
-    DB[<<~SQL].all.map { |status| PublishStatus.from_h(status) }
-      SELECT id, code, label
-      FROM publish_statuses
-    SQL
-  end
-
-  def find_publish_status_by_id(publish_status_id)
-    PublishStatus.from_h(DB[<<~SQL, publish_status_id].first)
-      SELECT id, code, label
-      FROM publish_statuses
-      WHERE id = ?
-    SQL
-  end
-
-  def insert_post(post)
-    post.set_id(
-      DB[:posts].insert({
-        title: post.title,
-        content: post.content,
-        publish_status_id: post.publish_status_id,
-        administrator_id: post.administrator_id,
-        created_at: post.created_at,
-        last_updated_at: post.last_updated_at,
-      })
-    )
-  end
-
-  def update_post(post)
-    DB[:posts].where({ id: post.id }).update({
-      title: post.title,
-      content: post.content,
-      publish_status_id: post.publish_status_id,
-      administrator_id: post.administrator_id,
-      last_updated_at: post.last_updated_at,
-    })
-  end
-
-  def delete_post_by_id(post_id)
-    DB[:posts].where({ id: post_id }).delete
   end
 end
